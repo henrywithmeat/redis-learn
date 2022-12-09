2b55f3f4d189068d84289662ea6991dac6389063
henrywang <1279544350@qq.com>
2022-12-09 15:30:36 +0800

    master commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,7 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("sth strange");
         System.out.println("new wave");
-        System.out.println("new thing");
+        System.out.println("new");
     }
 
 }
